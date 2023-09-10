4dc985acf4d8797c64689d3557338cec71709ea1
matiasmaciel2023 <144575397+matiasmaciel2023@users.noreply.github.com>
2023-09-10 00:32:47 -0300

    version1
    Todavia falta trabajar con JavaScript.

---
+++ javascript/enlaces.js
@@ -0,0 +1,12 @@
+
+
+//document.getElementById("hmc").onclick = window.location = "https://www.hmc.mil.ar/"
+
+//document.getElementById("hiba").onclick = window.location = "http://hospitalitaliano.org.ar/#!/home/principal"
+
+//document.getElementById("sg").onclick = window.location = "https://www.sg.com.ar/sitio/"
+
+//document.getElementById("chsjd").onclick = window.location.replace("https://sanjuandedios.org.ar/");//*
+
+
+
